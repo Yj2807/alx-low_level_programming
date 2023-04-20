c95800c4dcd4c4bbb2c20ea8442237647c88d01d
Yj2807 <jerrybirhanu6@gmail.com>
2023-04-20 18:48:12 +0300

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints with proper grammer, but the outcome is a piece of art
+* Return: 0 if excited prperly, non-zero otherwise
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
