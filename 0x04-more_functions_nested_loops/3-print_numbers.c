5278088bf8cea9e64d05dbc3c7add4874c48a957
Yj2807 <jerrybirhanu6@gmail.com>
2023-04-27 20:39:07 +0300

    c programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers between 0 to 9 incl.
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+
+	}
+	_putchar('\n');
+
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n : If size is 0 or less, the function should print only a new line
+ * Return: Void.
+ */
+
+void print_square(int n)
+
+{
+	int i = 0, ii;
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
